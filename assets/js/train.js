248d2f1695e75cd1768ef4447ffaaa1b3ed754bf
judith pizzolato <judith pizzolato>
2016-07-04 23:20:09 -0400

    added code for the calculations for the times and also code to populate html

--- assets/js/train.js
+++ assets/js/train.js
@@ -41,5 +41,36 @@ $("#addTrainBtn").on("click", function(){
 	//Determines when the next train arrives
 	return false;
 });
+
+//Adding trains to the database and in the rows of html when the user add an entry
+trainData.ref().on("child_added", function(childSnapShot, prevChildKey){
+
+	console.log(childSnapShot.val());
+
+//data stored in a variable
+
+var tName = childSnapShot.val().name;
+var tDestination = childSnapShot.val().destination;
+var tFrequency = childSnapShot.val().frequency;
+var tFirstTrain = childSnapShot.val().firstTrain;
+
+//Calculations of train minutes till arrival, the the current time and subtract the FirstTrain time and find the modulus between the difference and the frequency
+var differenceTimes = moment().diff(moment.unix(tFirstTrain),"minutes");
+var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency;
+var tMinutes = tFrequency - tRemainder;
+
+//Calculate the arrival time, add the tMinutes to the current time
+var tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+console.log(tMinutes);
+console.log(tArrival);
+
+console.log(moment().format("hh:mm A"));
+console.log(tArrival);
+console.log(moment().format("X"));
+
+//Adding each train data to the table
+$("#trainTable > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
+
+	});
 });
 
